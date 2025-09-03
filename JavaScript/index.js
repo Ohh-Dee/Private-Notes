5b10a685f85c0b55e9526f11bfec142e3bb5ad6f
Oscar D <oscarduran886@gmail.com>
2025-09-03 00:16:21 -0400

    Created JavaScript Folder with html, css, and js files to go along while creating a Title and text using document.getElementById().textContent function.

---
+++ JavaScript/index.js
@@ -0,0 +1,10 @@
+console.log(`Hello`);
+console.log(`Pizza Time`);
+// The following shows a pop up alert on the website and will be turned to comments for future use.
+//window.alert(`This is an alert!`);
+//window.alert(`I like pizza!`);
+
+// This is a comment
+
+document.getElementById("myH2").textContent = `Hello`;
+document.getElementById("myP").textContent = `This text was added using document.getElementByID().textContent in JavaScript. You may clap now.`
