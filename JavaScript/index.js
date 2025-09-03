42daec83db121ee3f51319c596620fae9e321048
Oscar D <oscarduran886@gmail.com>
2025-09-03 13:21:40 -0400

    Updated Notes in JavaScript and HTML files. Updated both the .js and .html to cover variables and console log inputs.

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -7,4 +7,58 @@ console.log(`Pizza Time`);
 // This is a comment
 
 document.getElementById("myH2").textContent = `Hello`;
-document.getElementById("myP").textContent = `This text was added using document.getElementByID().textContent in JavaScript. You may clap now.`
+document.getElementById("myP").textContent = `This text was added using document.getElementByID().textContent in JavaScript. 
+You may clap now. Please press F12 on the web browser in order to follow along in the console.`;
+
+// variable = A container that stores a value.
+            //Behaves as if it were the value it contains.
+
+
+// There are two elements to this
+// let x; is the declaration that presents the variable into the code
+// x = 100; is the assignment that allows your variable in this case x to equal 100.
+console.log(`>The Following will cover variables.<`)
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+
+
+
+console.log(typeof age);
+
+console.log(`You are ${age} years old`);
+console.log(`The price is $${price}`)
+console.log(`Your gpa is: ${gpa}`)
+
+
+let firstName = "Bobby";
+let lastName = "Hertz";
+
+//console.log(`Your name is ${firstName} ${lastName}`);
+
+// Strings are a series of character that can include numbers but cannot be used for math functions.
+
+let online = true;
+let forSale = true;
+console.log(`User is online: ${online}`);
+
+console.log(`Is this jeep for sale: ${forSale}`);
+
+let fullName = (`${firstName} ${lastName}`)
+let student = true
+
+console.log(fullName)
+
+document.getElementById("myP1").textContent = `Your name is ${fullName}.`;
+document.getElementById("myP2").textContent = `Your are ${age} years old.`;
+document.getElementById("myP3").textContent = `Enrolled: ${student}`;
+
+console.log(`>Following will Cover Arithmetic Operators.<`)
+
+// arithmetic operators = operands (values, variables, etc.)
+//                        operators (+, -, *, /)
+//                        
+
+
+
