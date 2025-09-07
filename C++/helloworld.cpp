f3e427c56b1afcc36e9ab06bd77f0cf52d4b201c
Oscar D <oscarduran886@gmail.com>
2025-09-07 02:13:34 -0400

    Established C++ folder and created welcome file.

---
+++ C++/helloworld.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+
+    // In order to write a comment you will need to use two "//" ingore the quotation marks.
+// For mutiline comments do the following below
+
+
+    std::cout << "I like Pizza!" << '\n'; // << std::endl is the same as \n but n is better performace wise.
+    std::cout << "It is really good!" << '\n'; // std::endl will flush the output buffer.
+    return 0;
+}
+
+/*
+This is an example 
+of a space 
+for mutiline notes
+*/
