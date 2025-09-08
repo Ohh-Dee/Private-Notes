90d1172a3fd318987841b6c17c9219de86353065
Oscar D <oscarduran886@gmail.com>
2025-09-07 23:06:10 -0400

    Created Arithmetic Operator Notes.

---
+++ C++/Keywords/ArithmeticOperators.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+/*
+Arithmetic Operators = return the result of a specific arithmetic operation (+ - * /)
+*/
+double students = 20;
+
+// the prefered way to add just one is to add ++ I.E. students++;
+/*
+Others:
+students--
+students *= 2
+students /= 2
+*/
+students+=1;
+
+
+
+std::cout << students << '\n';
+
+return 0;
+}
