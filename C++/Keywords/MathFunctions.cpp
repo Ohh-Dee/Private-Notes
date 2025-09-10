1d3903452977843d474ccbfc944c267dc542ae90
Oscar D <oscarduran886@gmail.com>
2025-09-10 00:08:21 -0400

    Added MathFunctions to notes and also created a Hypotenuse Calculator program.

---
+++ C++/Keywords/MathFunctions.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+
+double x = 3;
+double y = 4;
+double z;
+// Both of these functions do not require #include <cmath>
+// z = std::max(x, y); This will compare the two values and assign z with the max (highest)
+// z = std::min(x, y); This will compare the two values and assign z with the min (lowest)
+
+// z = pow(2, 3); This will raise the fisrt number (2), to the power of the second (3).
+// z = sqrt(9); This will give you the square root of a number.
+// z = abs(-5); This will give you the ABSOLUTE VALUE number in this example of -5 which is 5. This is the distance between the number and 0.
+// z = round(x); This would round a number
+// z = ceil(x): This would round up a number
+// z = floor(x); x rounded down = 3
+
+// Vist  htpps://www.cplusplus.com/reference/cmath/     for more math functions. 
+
+
+stud::cout << z;
+
+
+    return 0;
+}
