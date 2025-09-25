c87e89c9cf18d66ec30e5f25c6f7dc622f1f46c0
Oscar D <oscarduran886@gmail.com>
2025-09-25 18:06:31 -0400

    Created WGU Intro to IT notes.

--- C++/Keywords/variable-scope.cpp
+++ C++/Keywords/variable-scope.cpp
@@ -9,12 +9,18 @@ int main(){
 /*
 Local variables = declared inside a function or block {}
 Global variables = declared outside of all functions
-*/
 
-    printNum(myNum);
+Best to keep global variables to a min 
+more secure if you keep it within a function. 
+*/
+    int myNum = 1;
+    printNum();
+    // Putting :: before the myNum Tells the program to use the global version.
+    std::cout << ::myNum << '\n' << myNum << '\n';
     return 0;
 }
 
 void printNum(){
-    std::cout << myNum;
+    int myNum = 2;
+    std::cout << myNum << '\n';
 }
---
+++ Python/core-notes/type-casting.py
@@ -0,0 +1,9 @@
+# Typecasting = the process of converting a variable from one data type to another 
+#     str(), int(), float(), bool()
+
+name = "Oscar D"
+age = 25
+gpa = 3.2
+is_student = True
+
+print(type(gpa))
