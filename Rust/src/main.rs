6bb7784a3c457aadb21ba565348844fe46c05976
oscard <oscarduran886@gmail.com>
2026-02-10 00:11:06 -0500

    Added Hello World! with notes under rust folder.

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,3 +1,14 @@
+/*
+A rust program will typically have many functions.
+Function has their own role.
+No rules on how many functions it is all how you want to organize it
+every rust function starts with fn which tells rust we are starting a function
+then we insert the function name and every rust program must have a main function
+it will look like fn main(){}.
+() in fn main is the parameter which is an input to a function.
+This will run first always and this is the entry point to the program.
+Executing a function can be more easily referred to CALLING or INVOKING the function.
+*/
 fn main() {
     println!("Hello, world!");
 }
