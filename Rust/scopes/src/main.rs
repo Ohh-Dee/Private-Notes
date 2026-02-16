93fa55ca2e80266f790e8599e7d7fac98e979bcb
oscard <oscarduran886@gmail.com>
2026-02-16 02:47:39 -0500

    Finished adding notes on scopes main.rs

--- Rust/scopes/src/main.rs
+++ Rust/scopes/src/main.rs
@@ -3,5 +3,15 @@ fn main() {
     Scope is the boundary or region of code where a name is valid or capable of being used.
     Blocks is the area between an opening of curly braces and a closing curly brace.
     */
-    let _coffe_price = 5.99;
+    let _coffee_price = 5.99;
+    // so for coffee_price its scope ends within the main function.
+    {
+        // this is a nested scope which it can see _coffee_price because it is on the outside
+        // or parent scope but still within the fn main block function.
+        println!("Your Coffee will cost ${_coffee_price}.");
+        //once we reach the end of this nested scope any variable within this scope cannot be used
+        // as it ends in this inner scope.
+    }
+    // if you were to create another variable with the same name within a nested scope it will
+    // treat it as a unique variable and will use it first but the parent scope will still exist.
 }
