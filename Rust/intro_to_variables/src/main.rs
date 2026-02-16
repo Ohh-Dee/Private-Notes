224ba0b5228f2c0196d743c8d6ee2e99f194fd3e
oscard <oscarduran886@gmail.com>
2026-02-16 01:28:06 -0500

    Fixed format for comments under intro to variables main.rs

--- Rust/intro_to_variables/src/main.rs
+++ Rust/intro_to_variables/src/main.rs
@@ -3,10 +3,12 @@ fn main() {
     // to assign a var use let then the var name then = then the value
     let oranges = 14 + 6;
     let fruits = apples_num + oranges;
-/*
-You will want to use your variables first put in your string then input {} for dynamtic input then
-after the "" put a , and then put which variable you want to show up in order./
-*/
-    println!("This year my garden has {} apples and {} oranges for a total of {} fruits",
-             apples_num, oranges, fruits);
+    /*
+    You will want to use your variables first put in your string then input {} for dynamtic input
+    then after the "" put a , and then put which variable you want to show up in order./
+    */
+    println!(
+        "This year my garden has {} apples and {} oranges for a total of {} fruits",
+        apples_num, oranges, fruits
+    );
 }
